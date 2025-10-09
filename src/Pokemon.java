9fb802a74a6671b9a2f9362d098e5e9ec96574b0
Martin-fb-28 <martinfbao@gmail.com>
2025-10-09 13:54:59 +0200

    primer commit

---
+++ src/Pokemon.java
@@ -0,0 +1,53 @@
+public class Pokemon {
+    public static void main(String[] args) {
+
+        String pokemon = "Raichu"; String pokemon1 = "Bulbassaur";
+        int vida = 150; int vida2 = 125;
+        int MP1 = 70;   int MP2 = 45;
+        
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
