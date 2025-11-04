39039a8a6cb8194e3c38e83b5a5946fe7577ee19
Martin-fb-28 <martinfbao@gmail.com>
2025-11-04 09:50:29 +0100

    Ejercicio corregido

--- src/Pokemon.java
+++ src/Pokemon.java
@@ -2,31 +2,32 @@ import java.util.Scanner;
 
 public class Pokemon {
 
-    public static String estadopok1(){
-        int vida = 150;
-        int MP = 70;
-        int defensa = 10;
-        String estado = "La vida es: " + vida + "El MP es: " + MP + "La defensa es: " + defensa;
+    public static String estadopok1(int vida, String nombre, int defensa, int MP){
+        String estado = "\nLa vida de " + nombre + " es: " + vida + " El MP es: " + MP + " La defensa es: " + defensa;
         return estado;
     }
 
-    public static int queataque(){
+    public static int queataque(String nombre){
+        System.out.println("\nEs el turno de " + nombre);
+        System.out.println("\nEscoge el ataque: ");
+        System.out.println(" 1) Ataque normal ");
+        System.out.println(" 2) Ataque especial ");
         Scanner sc = new Scanner(System.in);
+        int vida = 150;
+        int vida2 = 135;
         int ataque;
         ataque = sc.nextInt();
         while(ataque != 1 && ataque != 2){
-            System.out.println("\nEso no es correcto, porfavor, introduce el ataque 1 o 2");
-            ataque = sc.nextInt();
-            System.out.println("\nHas escogido el ataque: " + ataque);
+                System.out.println("\nEso no es correcto, porfavor, introduce el ataque 1 o 2");
+                ataque = sc.nextInt();
+                System.out.println("\nHas escogido el ataque: " + ataque);
         }return ataque;
         }
 
-    public static int dmgatq(){
-        int dmg = 35;
-        int vidaenemy = 135;
-        int defenemy = 10;
-        int vidarest = (dmg - defenemy) - vidaenemy;
-        return vidarest;
+    public static int dmgatq(int dmg, int defenemy, int vidaenemy){
+        int vidafin;
+        vidafin = vidaenemy - (dmg - defenemy) ;
+        return vidafin;
     }
 
     public static void main(String[] args) {
@@ -49,10 +50,8 @@ public class Pokemon {
         int consumoatqespc2 = 40;
         String atqespecial2 = "Madre Naturaleza";
 
-        System.out.println(pokemon1 + " vida: " + vida1 + " MP: " + MP1 + " dmg: " + dmg1);
-        System.out.println(pokemon2 + " vida: " + vida2 + " MP: " + MP2 + " dmg: " + dmg2);
-        System.out.println("\n");
-        System.out.println("¡Comienza la batalla entre: " + pokemon1 + " y " + pokemon2 + "!");
+        System.out.println(estadopok1(vida1, pokemon1, defensa1, MP1));
+        System.out.println(estadopok1(vida2, pokemon2, defensa2, MP2));
 
         boolean turnoRaichu = true;
         boolean turnoBulbassaur = false;
@@ -61,20 +60,13 @@ public class Pokemon {
 
         while (vida1 > 0 && vida2 > 0) {
             if (turnoRaichu == true){
-                System.out.println("\nEs el turno de Raichu! ");
-                System.out.println("\nEscoge qué ataque quieres hacer: ");
-                System.out.println(" 1) Ataque normal ");
-                System.out.println(" 2) Ataque especial ");
-                int opcion = sc.nextInt();
-
+                int opcion = queataque(pokemon1);
                 if (opcion == 1) {
-                    int danhototal = dmg1 - defensa2;
-                    vida2 -= danhototal;
+                    vida2 = dmgatq(dmg1 , defensa2, vida2);
                     System.out.println(pokemon1 + " Utiliza rayo contra " + pokemon2);
                 } else if (opcion == 2) {
                     if(MP1 >= consumoatqespc) {
-                        int danhototal = dmgatqespc - defensa2;
-                        vida2 -= danhototal;
+                        vida2 = dmgatq(dmgatqespc , defensa2, vida2);
                         MP1 -= consumoatqespc;
                         System.out.println(pokemon1 + " Utiliza " + atqespecial);
                     }else if (MP1 <= consumoatqespc) {
@@ -85,21 +77,13 @@ public class Pokemon {
             turnoRaichu = !turnoRaichu;
             {
                 if (turnoBulbassaur == true) {
-                    System.out.println(" Es el turno de Bulbassaur! ");
-                    System.out.println("\nEscoge el ataque: ");
-                    System.out.println(" 1) Ataque normal ");
-                    System.out.println(" 2) Ataque especial ");
-                    Scanner sc1 = new Scanner(System.in);
-                    int opcion2 = sc.nextInt();
-
+                    int opcion2 = queataque(pokemon2);
                     if (opcion2 == 1) {
-                        int danhototal = dmg2 - defensa1;
-                        vida1 -= danhototal;
+                        vida1 = dmgatq(dmg2, defensa1, vida1 );
                         System.out.println(pokemon2 + " Utiliza Látigo contra Raichu ");
                     } else if (opcion2 == 2) {
                         if(MP2 >= consumoatqespc) {
-                            int danhototal = dmgatqespc2 - defensa1;
-                            vida1 -= danhototal;
+                            vida1 = dmgatq(dmgatqespc, defensa1, vida1);
                             MP2 -= consumoatqespc2;
                             System.out.println(pokemon2 + " Utiliza Madre Naturaleza contra Raichu ");
                         }else if (MP2 <= consumoatqespc2) {
